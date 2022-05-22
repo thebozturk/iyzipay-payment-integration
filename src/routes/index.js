a6179ff0d51021aa972980ad8728dd42134ae07b
Buğrahan Öztürk <70200551+thebozturk@users.noreply.github.com>
2022-05-22 23:48:27 +0300

    Add the installment functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import Test from './test.js'
 import Users from './users.js'
 import Cards from './cards.js'
+import Installment from './installment.js'
 export default [
     Test,
     Users,
-    Cards
+    Cards,
+    Installment
 ]
