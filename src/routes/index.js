dbafd69dda0f6596b9c52b5328b7ab1eeda1e8e8
Buğrahan Öztürk <70200551+thebozturk@users.noreply.github.com>
2022-06-12 17:53:01 +0300

    Add the checkout payment routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ import Cards from './cards.js'
 import Installment from './installment.js'
 import Payments from './payments.js'
 import PaymentsThreeDS from './payments-threeds.js'
+import Checkout from './checkout.js'
 export default [
     Test,
     Users,
     Cards,
     Installment,
     Payments,
-    PaymentsThreeDS
+    PaymentsThreeDS,
+    Checkout
 ]
